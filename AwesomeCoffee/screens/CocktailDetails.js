685428c8c55902f023fc477e1c77fe696f7ca4ca
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-11 20:33:29 +0200

    new change

---
+++ AwesomeCoffee/screens/CocktailDetails.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CocktailDetails = () => {
+  return (
+    <View>
+      <Text>CocktailDetails</Text>
+    </View>
+  )
+}
+
+export default CocktailDetails
+
+const styles = StyleSheet.create({})
