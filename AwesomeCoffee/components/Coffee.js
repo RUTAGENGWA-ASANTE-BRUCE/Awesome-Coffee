0a81109d42076eba504de8dd1815a696819c3a16
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-12 12:38:09 +0200

    new change

--- AwesomeCoffee/components/Coffee.js
+++ AwesomeCoffee/components/Coffee.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,SafeAreaView,TouchableOpacity,Image,TextInput,FlatList } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,TouchableOpacity,Image,TextInput,FlatList,Pressable } from 'react-native'
 import { LinearGradient } from "expo-linear-gradient";
 import React from 'react'
 import {SafeAreaProvider} from "react-native-safe-area-context"
@@ -14,10 +14,19 @@ const Coffee = () => {
             colors={["#3b3b40", "#26262b",]}
             style={tw`rounded-md w-44 p-2 h-60 `}
         >
+        {/* <View style={{backgroundImage:linear-gradient(to bottom right, rgba(255,255,255,0.2), rgba(255,255,255,0));}}/> */}
         <Image source={require("../assets/coffee1.jpeg")} style={tw`rounded-md w-full h-32`} />
         <Text style={tw`mt-1 text-white text-xl`}>Cappuccino</Text>
         <Text style={tw` text-gray-500 text-sm`}>With Oak mIlk</Text>
-        <Text style={tw`text-white text-lg`}><Icon name</Text>
+        <View style={tw`flex flex-row justify-between`}>
+
+        <Text style={tw`text-white text-lg`}>
+        <Text style={{color:"#ba572f"}}>$</Text> 4.20
+        </Text>
+        <Pressable style={{height:30,borderRadius:6,width:30,backgroundColor:"#ba572f"}}>
+            <Icon name="add" type="ionicon" color="white" style={tw`mt-0.5`}/>
+        </Pressable>
+        </View>
         </LinearGradient>
         </TouchableOpacity>
   )
