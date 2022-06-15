446d2468e650c13af71d383f9cec31415bc5435f
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-15 09:59:10 +0200

    steak Screen

---
+++ BonApettit/screens/Steak.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Steak = () => {
+  return (
+    <View>
+      <Text>Steak</Text>
+    </View>
+  )
+}
+
+export default Steak
+
+const styles = StyleSheet.create({})
