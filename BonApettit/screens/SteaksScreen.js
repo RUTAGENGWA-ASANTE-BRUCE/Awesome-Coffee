2ea523c639dab318935bfd269f5ae089ea1397a7
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-15 21:05:04 +0200

    new changes

--- BonApettit/screens/SteaksScreen.js
+++ BonApettit/screens/SteaksScreen.js
@@ -17,35 +17,53 @@ import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
 import Steak1 from "../assets/steak1.jpg";
 import Steak2 from "../assets/steak2.jpg";
+import Steak3 from "../assets/steak3.jpg";
+import Steak4 from "../assets/steak4.jpg";
 
-const steaks = [
+const Steaks = [
   {
     id: 452,
     image: Steak1,
-    ingredients: "Grilled steak with viggie & potatoes",
+    ingredients: "Grilled Steak with Stuffed Squash Blossoms & Crispy Onions",
     price: 4.5,
     calories: 300,
   },
+    {
+    id: 45522,
+    image: Steak2,
+    ingredients: "Ribeye steak with viggie & potatoes",
+    price: 7.76,
+    calories: 100,
+  },
+  {
+    id: 455352,
+    image: Steak3,
+    ingredients: "Grilled steak with viggie & potatoes",
+    price: 10.5,
+    calories: 400,
+  },
   {
     id: 7444,
-    image: Steak2,
-    ingredients: "Beef steak with viggie & potatoes",
+    image: Steak4,
+    ingredients: "Hanger steak with viggie & potatoes",
     price: 6.7,
-    calories: 200,
+    calories: 370,
   },
 ];
 const SteaksScreen = () => {
   const navigator = useNavigation();
-const dispatch=useDispatch()
+  const dispatch = useDispatch();
 
   return (
-    <ScrollView
+    <View
       style={{
         backgroundColor: "#26262b",
-        height: "100%",
         paddingTop: 20,
         paddingLeft: 15,
         paddingRight: 15,
+        display:"flex",
+        height:"100%",
+        flexDirection:"column"
       }}
     >
       <View style={tw`flex flex-row justify-between`}>
@@ -74,53 +92,59 @@ const dispatch=useDispatch()
           style={tw`w-10 h-10 rounded-full`}
         />
       </View>
-
-      <FlatList
-        data={steaks}
-        keyExtractor={(item) => item.id}
-        renderItem={({
-          item: { id, image, ingredients, price, calories },
-          item,
-        }) => (
-          <TouchableOpacity onPress={()=>{navigator.navigate("Steak");dispatch(setChoosenSteak(item))}}>
-            <BlurView
-              style={tw` rounded-3xl rounded-3xl   flex flex-col px-5     h-72 w-80 mx-auto mb-4  bg-red-200 mt-3 pt-2`}
-              tint="dark"
-              intensity={3390}
+        <FlatList
+          data={Steaks}
+          keyExtractor={(item) => item.id}
+          renderItem={({
+            item: { id, image, ingredients, price, calories },
+            item
+          }) => (
+            <TouchableOpacity
+              onPress={() => {
+                navigator.navigate("Steak");
+                dispatch(setChoosenSteak(item));
+              }}
             >
-              <Image
-                source={image}
-                style={{
-                  height: 160,
-                  width: 160,
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  borderRadius: 75,
-                }}
-              />
-              <View style={tw`flex flex-row justify-between mt-3 w-full mt-1`}>
-                <Text style={tw`text-white  text-xl font-semibold w-4/6`}>
-                  {ingredients}
-                </Text>
-                <View style={tw`flex justify-between flex-col `}>
-                  <View style={tw`flex flex-row`}>
-                    <Text style={tw`text-gray-400`}>{price} </Text>
-                    <Icon
-                      name={"star"}
-                      type="ionicon"
-                      color="darkorange"
-                      style={tw`mt-1`}
-                      size={13}
-                    />
+              <BlurView
+                style={tw` rounded-3xl rounded-3xl   flex flex-col px-5     h-72 w-80 mx-auto mb-4  bg-red-200 mt-3 pt-2`}
+                tint="dark"
+                intensity={3390}
+              >
+                <Image
+                  source={image}
+                  style={{
+                    height: 160,
+                    width: 160,
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    borderRadius: 75,
+                  }}
+                />
+                <View
+                  style={tw`flex flex-row justify-between mt-3 w-full mt-1`}
+                >
+                  <Text style={tw`text-white  text-lg font-semibold w-5/6`}>
+                    {ingredients}
+                  </Text>
+                  <View style={tw`flex justify-between flex-col `}>
+                    <View style={tw`flex flex-row`}>
+                      <Text style={tw`text-gray-400`}>{price} </Text>
+                      <Icon
+                        name={"star"}
+                        type="ionicon"
+                        color="darkorange"
+                        style={tw`mt-1`}
+                        size={13}
+                      />
+                    </View>
+                    <Text style={tw`text-gray-400`}>{calories} kcal</Text>
                   </View>
-                  <Text style={tw`text-gray-400`}>{calories} kcal</Text>
                 </View>
-              </View>
-            </BlurView>
-          </TouchableOpacity>
-        )}
-      />
-    </ScrollView>
+              </BlurView>
+            </TouchableOpacity>
+          )}
+        />
+    </View>
   );
 };
 
